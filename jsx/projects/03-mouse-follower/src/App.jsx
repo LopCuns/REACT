a899029822206bdcc271f02acd50fbe56d90add3
LopCuns <jorgelopezcuns@gmail.com>
2023-12-29 18:17:53 +0100

    mouseFollower proyecto 3 acabado

--- jsx/projects/03-mouse-follower/src/App.jsx
+++ jsx/projects/03-mouse-follower/src/App.jsx
@@ -1,12 +1,54 @@
 import './App.css'
-import { useEffect } from 'react'
+import { useEffect,useState } from 'react'
+import { clamp } from './utils/clamp'
+
+const  MouseFollower = () => {
+  const [enabled,setEnabled] = useState(false)
+  const [position,setPosition] = useState({ x:0,y:0 })
+  // USEEFFECT : https://es.react.dev/reference/react/useEffect
+  // Permite ejecutar código arbitrario cuando unos estados/variables (DEPENDENCIAS) cambian
+  // useEffect recibe dos parámetros
+  // UNA FUNCIÓN que se ejecuta cuando cambien las DEPENDENCIAS
+  // un arreglo de DEPENDENCIAS, o estados a los que React escuchará
+  useEffect(()=>{
+    // En el primer renderizado se ejecuta sólo la función de configuración
+    // Después de cada rederizado con dependencias cambiadas,rimero se ejecuta la función 
+    //de limpieza con los valores antiguos y despues la función de configuración
+    // Función para mover el cursor
+    function handleMove(e){
+      const { clientX,clientY } = e
+      // clamp para evitar que el componente se salga de la pantalla
+      setPosition({x: clamp(0,clientX,window.innerWidth), y: clamp(0,clientY,window.innerHeight)})
+    }
+    // Activar el seguimiento sólo si hemos activado la opción
+    if (enabled) window.addEventListener('pointermove',handleMove)
+    // La función del return se ejecutará cuando 
+    // El componente desaparezca
+    // Cambia la dependencia
+    return () =>  window.removeEventListener('pointermove',handleMove)
+  },[enabled])
+  return (
+    <>
+      <button
+      onClick={() => setEnabled(!enabled)}
+      >{enabled ? "Desactivar" : "Activar"} puntero
+      </button>
+    <div className='app-pointer' 
+    style={{
+      transform:`translate(${position.x}px,${position.y}px)`,
+      visibility:`${enabled?'visible':'hidden'}`,
+    }}></div>
+    </>
+  )
+
+}
 
 function App() {
-  useEffect(()=>{
-    console.log('Efecto')
-  },[])
   return (
-    <h1>Proyecto con useEffect</h1>
+    <main>
+        <h1>Proyecto con useEffect</h1>
+        <MouseFollower ></MouseFollower>
+    </main>
   )
 }
 
